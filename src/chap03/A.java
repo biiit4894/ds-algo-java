d33f0e01a85667d9f18f833694872bbbdb945c74
Hanbit <ghiesf@naver.com>
2024-04-11 21:59:49 +0900

    chapter03.검색 알고리즘 - 오름차순 배열에서 특정 데이터 찾기 예제

---
+++ src/chap03/A.java
@@ -0,0 +1,16 @@
+// package chap03;
+//
+// public class A implements Comparable<A> {
+// 	// 필드, 메소드 등
+//
+// 	public int compareTo(A c) {
+// 		// this가 c보다 크면 양의 값 반환
+// 		// this가 c보다 작으며 음의 값 반환
+// 		// this가 c와 같으면 0 반환
+// 	}
+//
+// 	public boolean equals(Object c) {
+// 		// this가 c와 같으면 true 반환
+// 		// this가 c와 같지 않으면 false 반환
+// 	}
+// }
---
+++ src/chap03/X.java
@@ -0,0 +1,16 @@
+// package chap03;
+//
+// public class X {
+// 	public static final Comparator<T> COMPARATOR = new Comp();
+// 	// 2. 그 후에 클래스의 인스턴스를 생성
+//
+// 	private static class Comp implements Comparator<T> {
+// 		// 1. Comparator 인터페이스와 compare 메서드를 구현한 클래스를 먼저 작성
+//
+// 		public int compare(T d1, T d2) {
+// 			// d1이 d2보다 크면 양수 반환
+// 			// d1이 d2보다 작으면 음수 반환
+// 			// d1이 d2와 같으면 0 반환
+// 		}
+// 	}
+// }
